4cca809149e4523a28df6d05ec7d218f47365c6e
Anish Programmer <70856239+86919@users.noreply.github.com>
2022-09-08 02:31:50 +0000

    While to Do-While loop

--- Main.java
+++ Main.java
@@ -7,21 +7,21 @@ import java.util.*;
 
 public class Main {
 	// Declaring COLOR_RESET so that we can reset the color
-    public static final String COLOR_RESET = "\u001B[0m";
-  
-    // Declaring the color
-    // Custom declaration
-    public static final String COLOR_BLUE = "	\u001B[34m";
-		public static final String COLOR_GREEN = "\u001B[32m";
-	
+	public static final String COLOR_RESET = "\u001B[0m";
+
+	// Declaring the color
+	// Custom declaration
+	public static final String COLOR_BLUE = "	\u001B[34m";
+	public static final String COLOR_GREEN = "\u001B[32m";
+
 	public static void main(String[] args) {
 		System.out.println("We welcome you to our Temperature Conversion PROGRAM! 🙋‍♂️");
 		System.out.println(COLOR_BLUE + "NOTE: You can do your operation INFINITELY 😊❕" + COLOR_RESET);
 
 		System.out.println(" ");
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Choose an option from below: ");
 		System.out.println("Press 1: for Kelvin - Celsius");
 		System.out.println("Press 2: for Celsius - Kelvin");
@@ -34,43 +34,57 @@ public class Main {
 
 		System.out.print("Enter your choice: ");
 		int user_Inp = input.nextInt();
-		
+
 		double val_KEL = 273.15; // Default value of KELVIN
-		while (user_Inp < 5)	{	
+		do {
 			switch (user_Inp) {
 				case 1:
 					System.out.println("KELVIN - CELSIUS");
 					System.out.println(" ");
 					System.out.print("Enter your KELVIN value: ");
-	
+
 					double temp_Kel = input.nextDouble();
-	
+
 					double change_Cel = temp_Kel - val_KEL; // Conversion: KELVIN - CELSIUS
-	
+
 					System.out.println(COLOR_GREEN + "➡ Temperature in CELSIUS is: " + change_Cel);
 					System.out.println(COLOR_RESET);
-	
-					break;
-	
-				case 2:
-					System.out.println("CELSIUS - KELVIN");
-	
-					System.out.println(" ");
-					System.out.print("Enter your CELSIUS value: ");
-					
-					double temp_Cel = input.nextDouble();
-	
-					double change_Kel = temp_Cel + val_KEL;
-					
-					System.out.println(COLOR_GREEN + "➡ Temperature in KELVIN is: " + change_Kel + COLOR_RESET);
-					System.out.println();
-	
+
 					break;
 
+				case 2:
+					System.out.println("CELSIUS - KELVIN");
+
+					System.out.println(" ");
+					System.out.print("Enter your CELSIUS value: ");
+
+					double temp_Cel = input.nextDouble();
+
+					double change_Kel = temp_Cel + val_KEL;
+
+					System.out.println(COLOR_GREEN + "➡ Temperature in KELVIN is: " + change_Kel + COLOR_RESET);
+
+					break;
+
+				case 3:
+					System.out.println("CELSIUS - FARENHEIT");
+
+					System.out.println(" ");
+					System.out.print("Enter your CELSIUS value: ");
+
+					double temp_Cel_1 = input.nextDouble();
+					double change_Faren = (temp_Cel_1 * 9 / 5) + 32;
+
+					System.out.println(COLOR_GREEN + "➡ Temperature in FARENHEIT is: " + change_Faren + COLOR_RESET);
+
+					break;
+
+				default:
+					System.out.println("You entered a wrong choise");
 			}
 
 			System.out.println();
-			
+
 			System.out.println("Choose an option from below: ");
 			System.out.println("Press 1: for Kelvin - Celsius");
 			System.out.println("Press 2: for Celsius - Kelvin");
@@ -83,6 +97,6 @@ public class Main {
 
 			System.out.print("Enter your choice: ");
 			user_Inp = input.nextInt();
-		}
+		} while (user_Inp < 10);
 	}
 }
